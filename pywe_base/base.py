72101051f66da4622d12dd8604abc9d28bc4f262
Brightcells <brightcells@gmail.com>
2017-08-29 12:32:03 +0800

    :sparkles: Add param toencoding/tojson/res_processor_func

--- pywe_base/base.py
+++ pywe_base/base.py
@@ -9,14 +9,20 @@ class BaseWechat(object):
         self.OPEN_DOMAIN = 'https://open.weixin.qq.com'
         self.MCH_DOMAIN = 'https://api.mch.weixin.qq.com'
 
-    def get(self, url, verify=False, **kwargs):
+    def get(self, url, verify=False, toencoding=True, tojson=True, res_processor_func=None, **kwargs):
         # When ``verify=True`` and ``cacert.pem`` not match ``https://xxx.weixin.qq.com``, will raise
         # SSLError: [Errno 1] _ssl.c:510: error:14090086:SSL routines:SSL3_GET_SERVER_CERTIFICATE:certificate verify failed
         res = requests.get(url.format(**kwargs), verify=verify)
-        res.encoding = 'utf-8'
-        return res.json()
+        if toencoding:
+            res.encoding = 'utf-8'
+        if res_processor_func:
+            return res_processor_func(res)
+        return res.json() if tojson else res
 
-    def post(self, url, verify=False, **kwargs):
+    def post(self, url, verify=False, toencoding=True, tojson=True, res_processor_func=None, **kwargs):
         res = requests.post(url, verify=verify, **kwargs)
-        res.encoding = 'utf-8'
-        return res.json()
+        if toencoding:
+            res.encoding = 'utf-8'
+        if res_processor_func:
+            return res_processor_func(res)
+        return res.json() if tojson else res
